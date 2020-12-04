f0ce669fc11f736d33f362a79e007d98aa5ee93e
yoston <yostoncornet@hotmail.es>
2020-12-03 23:28:18 -0500

    Splash

---
+++ app/src/main/java/com/example/recorrido_buses/Login.java
@@ -0,0 +1,14 @@
+package com.example.recorrido_buses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
