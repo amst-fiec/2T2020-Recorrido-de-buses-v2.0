bc300a0b5865b53ffc89a1b1859d265e2af0f6de
carftorr <73551328+carftorr@users.noreply.github.com>
2021-01-01 21:18:12 -0500

    Paradas

--- app/src/main/java/com/example/recorrido_buses/Mapa.java
+++ app/src/main/java/com/example/recorrido_buses/Mapa.java
@@ -1,8 +1,8 @@
 package com.example.recorrido_buses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,17 +10,28 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Mapa extends FragmentActivity implements OnMapReadyCallback {
     Switch switchButton;
     private GoogleMap mMap;
+    private DatabaseReference db_reference;
+    private ArrayList<Marker> tmpRealTimeMarkers=new ArrayList<>();
+    private ArrayList<Marker> realTimeMarkers=new ArrayList<>();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +40,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        db_reference = FirebaseDatabase.getInstance().getReference();
     }
 
     public void toParadas(View view) {
@@ -48,11 +60,28 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        db_reference.child("Parada").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (Marker marker:realTimeMarkers){
+                    marker.remove();
+                }
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    MapsCoor mc = snapshot.getValue(MapsCoor.class);
+                    Double lat = mc.getLat();
+                    Double lon = mc.getLon();
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(new LatLng(lat, lon));
+                    tmpRealTimeMarkers.add(mMap.addMarker(markerOptions));
+                }
+                realTimeMarkers.clear();
+                realTimeMarkers.addAll(tmpRealTimeMarkers);
+            }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
     public void switchButton(View view) {
